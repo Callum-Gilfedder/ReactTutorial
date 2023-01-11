fef8ad34905d590fb8dc52c0cd0f8e2763e1e2a2
Callum Gilfedder <Callumgilf@mail.com>
2023-01-11 09:56:03 +0000

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js'
+
+
 
 function App() {
   let x = "Hello world alt"; 
+  const link = "https://google.com";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        {x}
+        { x }
       </div>
+      <a href={link}>Link</a> 
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+
+function Navbar() {
+    return (
+        <nav className="navbar"> 
+            <div className="links">
+                <a hreh="/">Home</a>
+                <a hreh="/create">New post</a> 
+                <a hreh="">link3</a>
+                <a hreh="">link4</a>
+            </div>
+        </nav>  
+    )
+}
+
+export default Navbar;
