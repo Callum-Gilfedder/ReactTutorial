51265d6ca290052ec15afe1e3ed607fac09f2d6c
Callum Gilfedder <Callumgilf@mail.com>
2023-01-12 08:58:11 +0000

    Added a function in component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import Navbar from './Navbar.js'
 
 
 function App() {
+  const handleClick = () => {
+    console.log("function working correctly")
+  }
+
+
   return (
     <div className="App">
       <Navbar />
       <h1>
         Content
       </h1>
+      <button onClick={handleClick}>Test button</button>
     </div>
   );
 }
