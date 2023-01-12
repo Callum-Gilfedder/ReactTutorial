2f094e78b0ce2ced1f1a6a10ac659d8affbdb4ef
Callum Gilfedder <Callumgilf@mail.com>
2023-01-12 09:28:47 +0000

    Added useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import Navbar from './Navbar.js'
-
+import { useState } from 'react'
 
 
 function App() {
-  const handleClick = () => {
-    console.log("function working correctly")
-  }
+  const [name, setName] = useState("initial-state")
+  const [age, setAge] = useState("18") 
 
+  const handleClick = () => {
+    setName("altered-state")
+    setAge("20")
+  }
 
   return (
     <div className="App">
       <Navbar />
       <h1>
-        Content
+        { name } { age }
       </h1>
       <button onClick={handleClick}>Test button</button>
     </div>
